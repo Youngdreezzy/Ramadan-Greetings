a366dbff757bada7abe86eedd398cdf14ee309aa
Dreezzy <afolabiopesimon@gmail.com>
2023-04-30 22:20:13 +0100

    Seasons-greeting

---
+++ greetings.py
@@ -0,0 +1,12 @@
+# Names input
+first_name= input("Enter Your First Name:-")
+last_name= input("Enter Your Last Name:-")
+
+# Merging the first name and last name
+names= first_name + (" ") + last_name
+
+print("Due to the information being provided by you, your name is:-", names)
+print("Here is a greeting message customized to you:")
+print("   Hi", names, "Ramadan Mubarak to you and your loved ones.\n   May the celebrations of this Holy month spread happiness and joys in your life.\n   May Allah bring you all the inspiration in life and motivate you to achieve all your goals.\n   Wishing a very Happy Ramadan to you.")
+print("Salam ✌🏽")
+print("^Season Greetings From Youngdreezzy^")
